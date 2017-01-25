28e4c9b8aec0d90af2bd121e5842984f3bf7cdca
franky.zhu <franky.zhu@oceanwing.com>
2017-01-25 10:21:35 +0800

    replace time.Sub() with time.Since()

--- basic/args.go
+++ basic/args.go
@@ -31,14 +31,14 @@ func concatString(n int) float64 {
 	for _, s := range strs {
 		str += s
 	}
-	return time.Now().Sub(start).Seconds()
+	return time.Since(start).Seconds()
 }
 
 func joinString(n int) float64 {
 	strs := initStrings(n)
 	start := time.Now()
 	strings.Join(strs, "")
-	return time.Now().Sub(start).Seconds()
+	return time.Since(start).Seconds()
 }
 
 var n = flag.Int("n", 10000, "loop")
