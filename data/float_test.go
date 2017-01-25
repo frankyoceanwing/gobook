22a54569b9ce9722309cd6ab0ce74729eb821536
franky.zhu <franky.zhu@oceanwing.com>
2017-01-25 15:53:00 +0800

    float comparision

---
+++ data/float_test.go
@@ -0,0 +1,23 @@
+package data
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestFloatCompare(t *testing.T) {
+
+	Convey("Test float", t, func() {
+		a := 0.1 // float64
+		Convey("When b's type is float64", func() {
+			b := 0.1
+			So(a == b, ShouldBeTrue)
+		})
+
+		Convey("When b's type is float32 ", func() {
+			var b float32 = 0.1
+			So(a == float64(b), ShouldBeFalse)
+		})
+	})
+}
